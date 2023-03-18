d84e48f336328e1eedb1c5fec39364efc83ae775
Mohammad Rahman <me@shadikur.com>
2023-03-18 21:39:28 +0100

    Update

--- app.js
+++ app.js
@@ -53,11 +53,11 @@ const showMeal = (category) => {
 
 // Display Meals
 const displayMeals = (meals) => {
-    console.log(meals);
+  console.log(meals);
 
-    const mealsDiv = document.getElementById("category-container");
-    mealsDiv.innerHTML = "";
-    meals.forEach((meal) => {
+  const mealsDiv = document.getElementById("category-container");
+  mealsDiv.innerHTML = "";
+  meals.forEach((meal) => {
     const mealDiv = document.createElement("div");
     mealDiv.className = "meal";
     const mealInfo = `
@@ -67,12 +67,10 @@ const displayMeals = (meals) => {
         </a>
         <div class="p-5">
             <a href="#">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${
-                  meal.strMeal
-                }</h5>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${meal.strMeal}</h5>
             </a>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Meal ID: ${meal.idMeal}</p>
-            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onclick="showDetails('${meal.idMeal}')">
+            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" id="show-details-btn" onclick="showDetails('${meal.idMeal}')">
                 Show Details
                 <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
@@ -87,17 +85,35 @@ const displayMeals = (meals) => {
 
 // Show Details
 const showDetails = (idMeal) => {
-    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
-    fetch(url)
-        .then((res) => res.json())
-        .then((data) => displayDetails(data.meals[0]));
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayDetails(data.meals[0]));
 };
+
+
+// Modal show details
+const modal = document.querySelector('.modal');
+const modalClose = modal.querySelector('.modal-close');
 
 // Display Details
 const displayDetails = (meal) => {
-    console.log(meal);
-    console.log(meal.strYoutube);
-    const detailsDiv = document.getElementById("category-container");
+  console.log(meal);
+  console.log(meal.strYoutube);
+  modal.classList.remove('hidden');
+
+    // Hide modal when "Close" button is clicked
+    modalClose.addEventListener('click', () => {
+      modal.classList.add('hidden');
+    });
+  
+    // Hide modal when overlay is clicked
+    modal.addEventListener('click', (event) => {
+      if (event.target === modal) {
+        modal.classList.add('hidden');
+      }
+    });
+  
 };
 
 
